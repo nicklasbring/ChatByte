a0b4cce8aa8de5f088c4526c195078c4e91a745f
Cosby1992 <Ande016j@edu.easj.dk>
2019-08-28 17:43:38 +0200

    Server.fxml changed to server_gui.fxml. Added GUI for server, no function only visual. Method added in controller from server.fxml

--- src/ServerGui.java
+++ src/ServerGui.java
@@ -1,3 +1,11 @@
+import javafx.event.ActionEvent;
+
 public class ServerGui {
+
+
+    //Start Server button onAction
+    //Runs when start server button is pressed on server gui
+    public void startServer(ActionEvent actionEvent) {
+    }
 
 }

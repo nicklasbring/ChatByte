350afa19b5cb4746a3233c467b3c1c7b4ee7633f
Nicklas Nielsen <nicklas.bring.nielsen@gmail.com>
2019-08-28 10:08:08 +0200

    Lavet starten på Server Klassen

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,43 @@
-public class Server {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server implements Runnable{
+
+    //Klassevariabler
+    private int port;
+    ServerSocket server;
+    Socket socket;
+    DataOutputStream output;
+    DataInputStream input;
+
+    //No arg constructor
+    public Server() {
+
+    }
+
+    //Run metode som bliver overskrevet fra Runnable klassen
+    // Den bliver brugt til at køre serveren i en anden tråd
+    @Override
+    public void run() {
+        try {
+
+            //Initialiserer serveren på en angivet port
+            server = new ServerSocket(port);
+            //Accepterer hvis en klient prøver at tilslutte til serveren
+            socket = server.accept();
+
+            //Initialiserer datainput- og dataoutputstream for at kunne kommunikere mellem server og klient
+            input = new DataInputStream(socket.getInputStream());
+            output = new DataOutputStream(socket.getOutputStream());
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }

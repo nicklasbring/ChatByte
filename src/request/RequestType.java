edf6170ca8ed6e3e95bca53c011e68e4a82dcb2f
Cosby1992 <Ande016j@edu.easj.dk>
2019-09-02 14:07:22 +0200

    Message and requesttypes moved to package. Clienthandler small addition.

---
+++ src/request/RequestType.java
@@ -0,0 +1,5 @@
+package request;
+
+public enum RequestType {
+    MESSAGE, NEW_ROOM_REQUEST, ROOM_REQUEST, PRIVATE_REQUEST
+}
--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,5 +1,9 @@
 package server;
 
+import request.Message;
+import request.RequestType;
+
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -11,7 +15,6 @@ public class ClientHandler implements Runnable {
     private final ObjectInputStream ois;
     private final ObjectOutputStream oos;
     private final Socket socket;
-    private Vector<ChatRoom> connectedRooms;
 
     //Constructor
     ClientHandler(Socket socket, ObjectInputStream ois, ObjectOutputStream oos) {
@@ -19,17 +22,38 @@ public class ClientHandler implements Runnable {
         this.ois = ois;
         this.oos = oos;
 
-        connectedRooms = new Vector<>();
-
     }
 
     //Run method that handles client actions
     @Override
     public void run() {
 
+        Message request;
+
 
         while (true){
 
+            try {
+                request = (Message) ois.readObject();
+
+                switch (request.getType()){
+                    case MESSAGE:
+                        break;
+                    case NEW_ROOM_REQUEST:
+                        break;
+                    case ROOM_REQUEST:
+                        break;
+                    case PRIVATE_REQUEST:
+                }
+
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
 
 
         }

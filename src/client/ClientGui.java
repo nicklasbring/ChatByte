fdc99f84ca65a0af8d103b3a47bdedc7b3448d1e
MathiasAmi <math35i0@edu.easj.dk>
2019-09-02 11:24:33 +0200

    Client gui and controller

--- src/client/ClientGui.java
+++ src/client/ClientGui.java
@@ -1,4 +1,17 @@
 package client;
 
+import javafx.event.ActionEvent;
+
 public class ClientGui {
+    public void startPrivateChat(ActionEvent actionEvent) {
+    }
+
+    public void addRoom(ActionEvent actionEvent) {
+    }
+
+    public void addFriend(ActionEvent actionEvent) {
+    }
+
+    public void writeMessage(ActionEvent actionEvent) {
+    }
 }
